a394b37ce2e9e86d186c9b4894f3a7fc96a971e0
Tron-G <1285771266@qq.com>
2020-08-05 20:09:25 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
+<script>
+import MainTabBar from "components/contents/maintabbar/MainTabBar";
+export default {
+  name: "App",
+  components: {
+    MainTabBar
+  }
+};
+</script>
+
 <style>
+@import "assets/css/base.css";
 </style>
