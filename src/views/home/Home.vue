87a79ea4a4db8632e578d36e966e416ea6120c9d
Tron-G <1285771266@qq.com>
2020-09-03 23:35:42 +0800

    feature

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -10,18 +10,51 @@
       class="swiper"
       @swiperImgLoaded="swiperImgLoad"
     ></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/contents/tabControl/TabControl";
+
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
 import { getHomeMultidata } from "network/home.js";
 export default {
   name: "Home",
   components: {
     NavBar,
-    HomeSwiper
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -32,8 +65,6 @@ export default {
   created() {
     this.getHomeData();
   },
-
-  computed: {},
   methods: {
     getHomeData() {
       getHomeMultidata().then(res => {
@@ -50,9 +81,10 @@ export default {
 
 <style scoped>
 #home {
-  /* padding-top: 44px; */
+  padding-top: 44px;
   height: 100vh;
-  position: relative;
+  /* overflow: hidden; */
+  /* position: relative; */
 }
 
 .home-nav {
@@ -66,12 +98,12 @@ export default {
   z-index: 9;
 }
 .swiper {
-  position: absolute;
+  position: relative;
   overflow: hidden;
-  top: 44px;
+  /* top: 44px; */
   /* bottom: 44px; */
-  right: 0;
-  left: 0;
+  /* right: 0;
+  left: 0; */
   /* height: 300px; */
 }
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="recommend">
+    <div
+      v-for="(item, index) in recommends"
+      :key="index"
+      class="recommend-item"
+    >
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div v-cloak>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  components: {},
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;
+  width: 100vw;
+  text-align: center;
+  font-size: 12px;
+  padding: 15px 0 30px;
+  /* border-bottom: 8px solid #eee; */
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 90%;
+  margin-bottom: 5px;
+  /* height: 65px; */
+}
+
+[v-cloak] {
+  display: none;
+}
+</style>
